11256250e584c2fc2358468227ddb50900dab8d4
DeOldSax <DeOldSax@gmx.de>
2015-03-01 17:07:06 +0100

    Added insertion sort

--- js/script.js
+++ js/script.js
@@ -1,17 +1,70 @@
+a = [];
 $(document).ready(function() {
-    var a = [9, 3, 8, 2, 10, 11, 17, 4, 0, 33, 17, 3];
 
     $('#insertionsort').click(function() {
-        startInsertionSort(a);
+        visualizeInsertionSort(a, 500);
     });
+
+    setUpSortContainer(a);
+    initializeRandomizeButton();
 
 });
 
-function startInsertionSort(a) {
+function initializeRandomizeButton() {
+    $('#randomize').click(function() {
+        setUpSortContainer();
+    });
+}
+
+
+function setUpSortContainer() {
+    $('.bar').remove();
+
+    a = [];
+
+    for (var i = 0; i < 20; i++) {
+        a.push(Math.floor(Math.random()*50));
+    }
+
+    var containerHeight = $(window).height() - 200;
+    var containerWidth = $(window).width();
+    var maxBarHeight = containerHeight - 10;
+
+    $('#sortContainer').height(containerHeight);
+
+    // put bars on screen;
+    var maxValue = Math.max.apply(null, a);
+    var nextBar;
+    var previousBar;
+    var height;
+    for (var i = 0; i < a.length; i++) {
+        height = ( a[i] / maxValue ) * maxBarHeight;
+        if ( height == 0 ) {
+            height = 1;
+        }
+
+        nextBar = $('<span>').text(a[i]).addClass('bar').addClass('normal').height(height);
+        nextBar.css({ 'margin-top': maxBarHeight - height });
+
+        if (i == 0) {
+            $('#sortContainer').prepend(nextBar);
+        } else {
+            nextBar.insertAfter(previousBar);
+        }
+
+        previousBar = nextBar;
+    }
+}
+
+delay = 1000;
+function visualizeInsertionSort(a, delayTime) {
+    delay = delayTime;
+    valueToInsert = 0;
+    pickNextValue(a);
+
+/*
     console.log(a.join(", "));
-
     var valueToInsert;
-
     for (var i = 1; i < a.length; i++) {
         valueToInsert = i;
 
@@ -23,6 +76,63 @@ function startInsertionSort(a) {
 
     console.log(a.join(", "));
     console.log("swaps: " + swaps);
+*/
+}
+
+valueToInsert = 0;
+function pickNextValue(a) {
+    valueToInsert++;
+    console.log("INDEX=" + valueToInsert);
+
+    if (valueToInsert < a.length) {
+       insertNextValue(a);
+    } else {
+        unhighlightBars();
+        // finished, everything is sorted :)
+        console.log(a.join(", "));
+    }
+}
+
+currentHighlightedBar = -1;
+function insertNextValue(a) {
+    console.log("insertNextValue");
+    // highlight the associated bar to insert
+    unhighlightBars();
+    highlightBar(valueToInsert);
+
+    setTimeout(function() {
+        checkSwap(a, valueToInsert);
+    }, delay);
+}
+
+function checkSwap(a, index) {
+    console.log("checkSwap");
+    if (index > 0 && a[index-1] > a[index]) {
+        // highlightBar(valueToInsert);
+        // highlightBar(valueToInsert-1);
+
+        setTimeout(function() {
+            visualizeSwap(a, index);
+        }, delay);
+
+    } else {
+        pickNextValue(a);
+    }
+}
+
+function visualizeSwap(a, index) {
+    console.log("visualizeSwap");
+    swap(a, index-1, index);
+
+    // visualization
+    swapBar(index-1, index);
+    updatedDisplayedSwap();
+
+    index--;
+
+    setTimeout(function() {
+        checkSwap(a, index);
+    }, delay);
 }
 
 swaps = 0;
@@ -31,4 +141,45 @@ function swap(a, i, j) {
     a[i] = a[j];
     a[j] = t;
     swaps++;
+}
+
+function updatedDisplayedSwap() {
+    $('#swaps').text(swaps);
+}
+
+function highlightBar(i) {
+    if (i < 0) console.log("index out of bounds");
+    var children = $('#sortContainer').children(':nth-child(' + (i+1) + ')');
+    children.removeClass('normal').addClass('highlighted');
+}
+
+function unhighlightBars() {
+    $('.bar').removeClass('highlighted').addClass('normal');
+}
+
+function swapBar(i, j) {
+    var b1 = i + 1;
+    var b2 = j + 1;
+
+    var bar1 = $('#sortContainer').children(':nth-child(' + b1 + ')');
+    var bar2 = $('#sortContainer').children(':nth-child(' + b2 + ')');
+
+    var offset1;
+    var offset2;
+
+    if (i < j) {
+        offset1 = { left: "40px" };
+        offset2 = { left: "-40px" };
+    } else {
+        offset1 = { left: "-40px" };
+        offset2 = { left: "+40px" };
+    }
+
+    bar1.animate(offset1, delay);
+    bar2.animate(offset2, { duration: delay , complete: function() {
+        bar1.insertAfter(bar2);
+        bar1.css({ left: 0 });
+        bar2.css({ left: 0 });
+    }});
+
 }
